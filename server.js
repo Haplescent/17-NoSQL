af309660617c69a7cd427500f70a8b86d78214a1
John Merritt <johnmerritt95@gmail.com>
2020-08-03 11:03:45 -0700

    debugging deployed server

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ mongoose.connection.on("error", (err) => {
 mongoose.connection.on("connected", (err, res) => {
   console.log("mongoose is connected");
 });
+console.log(`remote db is at ${process.env.MONGODB_URL}`);
 console.log(`connected to ${MONGODB_URI}`);
 
 const PORT = process.env.PORT || 3000;
